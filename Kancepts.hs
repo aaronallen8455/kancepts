118a6f31954cdba6555bd12659651e8ea8cacf44
Aaron Allen <aaron@flipstone.com>
2021-03-28 16:14:08 -0500

    product and coproduct

--- Kancepts.hs
+++ Kancepts.hs
@@ -23,6 +23,7 @@ class Category cat where
   tgt :: cat a b -> Obj cat b
   (.) :: cat b c -> cat a b -> cat a c
 
+-- Identify objects with their identity morphism
 type Obj cat a = cat a a
 
 -- Category with a single object
@@ -34,9 +35,9 @@ instance Category One where
   tgt One = One
   One . One = One
 
--- Discrete category of two objects
 data ElemA = ElemA
 data ElemB = ElemB
+-- Discrete category of two objects
 data Two a b where
   A :: Two ElemA ElemA
   B :: Two ElemB ElemB
@@ -92,34 +93,35 @@ instance Category c => Functor (Identity c) where
 -- Natural Transformation
 --------------------------------------------------------------------------------
 
-type Nat :: (Type -> Type -> Type) -- Codomain category
-         -> (Type -> Type -> Type) -- Domain category
+type Nat :: (Type -> Type -> Type) -- Domain category
+         -> (Type -> Type -> Type) -- Codomain category
          -> Type -> Type -> Type
-data Nat c d g f where
+data Nat d c f g where
   Nat :: (Functor f, Functor g, c ~ Cod f, c ~ Cod g, d ~ Dom f, d ~ Dom g)
-      => g -> f -> (forall z. Obj d z -> c (f :% z) (g :% z)) -> Nat c d g f
+      => f -> g -> (forall z. Obj d z -> c (f :% z) (g :% z)) -> Nat d c f g
 
-type f ~> g = (Cod f ~ Cod g, Dom f ~ Dom g) => Nat (Cod f) (Dom f) g f
+type f ~> g = (Cod f ~ Cod g, Dom f ~ Dom g) => Nat (Dom f) (Cod f) f g
 
-vertComp :: Nat c d h g -> Nat c d g f -> Nat c d h f
-vertComp (Nat h _ n1) (Nat _ f n2) = Nat h f $ \o -> n1 o . n2 o
+vertComp :: Nat d c g h -> Nat d c f g -> Nat d c f h
+vertComp (Nat _ h n1) (Nat f _ n2) = Nat f h $ \o -> n1 o . n2 o
 
-instance Category (Nat c d) where
-  src (Nat _ d _) = Nat d d _
-  tgt _ = undefined -- Nat Identity Identity _
-  (.) = undefined
+instance Category (Nat d c) where
+  src (Nat d _ _) = Nat d d (d %)
+  tgt (Nat _ c _) = Nat c c (c %)
+  (.) = vertComp
 
 whiskerLeft :: (Functor h, Dom h ~ e, Cod h ~ d)
-            => Nat c d g f -> h -> Nat c e (g :.% h) (f :.% h)
-whiskerLeft (Nat g f c) h = Nat (Comp g h) (Comp f h) $ \o -> c $ h % o
+            => Nat d c f g -> h -> Nat e c (f :.% h) (g :.% h)
+whiskerLeft (Nat f g c) h = Nat (Comp f h) (Comp g h) $ \o -> c $ h % o
 
 whiskerRight :: (Functor h, Cod h ~ e, Dom h ~ c)
-             => Nat c d g f -> h -> Nat e d (h :.% g) (h :.% f)
-whiskerRight (Nat g f c) h = Nat (Comp h g) (Comp h f) $ \o -> h % c o
+             => Nat d c f g -> h -> Nat d e (h :.% f) (h :.% g)
+whiskerRight (Nat f g c) h = Nat (Comp h f) (Comp h g) $ \o -> h % c o
 
-horizComp :: Nat e c i h -> Nat c d g f -> Nat e d (i :.% g) (h :.% f)
-horizComp n1@(Nat _ h _) n2@(Nat g _ _) =
-  vertComp (whiskerLeft n1 g) (whiskerRight n2 h)
+-- Horizontal composition also forms a category but we can't express it here
+horizComp :: Nat c e h i -> Nat d c f g -> Nat d e (h :.% f) (i :.% g)
+horizComp n1@(Nat h _ _) n2@(Nat _ g _) =
+  whiskerLeft n1 g . whiskerRight n2 h
 
 --------------------------------------------------------------------------------
 -- Kan Extensions
@@ -134,7 +136,7 @@ newtype LanUP f k lan =
 
 data LeftKanExt k f lan where
   Lan :: lan
-      -> Nat (Cod f) (Dom f) (lan :.% k) f -- f ~> (lan :.% k)
+      -> f ~> (lan :.% k)
       -> LanUP f k lan
       -> LeftKanExt k f lan
 
@@ -147,7 +149,7 @@ newtype RanUP f k ran =
 
 data RightKanExt k f ran where
   Ran :: ran
-      -> Nat (Cod f) (Dom f) f (ran :.% k)
+      -> (ran :.% k) ~> f
       -> RanUP f k ran
       -> RightKanExt k f ran
 
@@ -168,7 +170,7 @@ instance Category c => Functor (ConstOne c) where
   ConstOne % _ = One
 
 colimit :: colimit
-        -> Nat (Cod f) (Dom f) (colimit :.% ConstOne (Dom f)) f
+        -> Nat (Dom f) (Cod f) f (colimit :.% ConstOne (Dom f))
         -> LanUP f (ConstOne (Dom f)) colimit
         -> LeftKanExt (ConstOne (Dom f)) f colimit
 colimit = Lan
@@ -184,7 +186,7 @@ instance Functor TwoToHask where
   TwoToHask % A = id
   TwoToHask % B = id
 
--- A constant functor from 1 to Hask selecting the coproduct of the elements of 2
+-- A constant functor from 1 to Hask selecting the coproduct of 2 objects
 data Coproduct = Coproduct
 
 instance Functor Coproduct where
@@ -196,25 +198,25 @@ instance Functor Coproduct where
 coproduct :: LeftKanExt (ConstOne Two) TwoToHask Coproduct
 coproduct = colimit Coproduct eta sigma
   where
-    eta :: Nat (->) Two (Coproduct :.% ConstOne Two) TwoToHask
-    eta = Nat (Comp Coproduct ConstOne) TwoToHask $ \case
-      A -> const $ Left ElemA
-      B -> const $ Right ElemB
+    eta :: Nat Two (->) TwoToHask (Coproduct :.% ConstOne Two)
+    eta = Nat TwoToHask (Comp Coproduct ConstOne) $ \case
+      A -> Left
+      B -> Right
 
     sigma :: LanUP TwoToHask (ConstOne Two) Coproduct
     sigma = LanUP $ \case
-      Nat (Comp g ConstOne) TwoToHask c -> Nat g Coproduct $ \case
+      Nat TwoToHask (Comp g ConstOne) c -> Nat Coproduct g $ \case
         One -> either (c A) (c B)
 
 -- Likewise, the right kan extension defines the limit
 
 limit :: limit
-      -> Nat (Cod f) (Dom f) f (limit :.% ConstOne (Dom f))
+      -> Nat (Dom f) (Cod f) (limit :.% ConstOne (Dom f)) f
       -> RanUP f (ConstOne (Dom f)) limit
       -> RightKanExt (ConstOne (Dom f)) f limit
 limit = Ran
 
--- Constant functor from 1 to Hask selecting the product of the elements of 2
+-- Constant functor from 1 to Hask selecting the product of 2 objects
 data Product = Product
 
 instance Functor Product where
@@ -226,13 +228,13 @@ instance Functor Product where
 product :: RightKanExt (ConstOne Two) TwoToHask Product
 product = limit Product mu delta
   where
-    mu :: Nat (->) Two TwoToHask (Product :.% ConstOne Two)
-    mu = Nat TwoToHask (Comp Product ConstOne) $ \two (a, b) ->
+    mu :: Nat Two (->) (Product :.% ConstOne Two) TwoToHask
+    mu = Nat (Comp Product ConstOne) TwoToHask $ \two (a, b) ->
       case two of
         A -> a
         B -> b
 
     delta :: RanUP TwoToHask (ConstOne Two) Product
     delta = RanUP $ \case
-      Nat TwoToHask (Comp g ConstOne) c -> Nat Product g $ \One z ->
+      Nat (Comp g ConstOne) TwoToHask c -> Nat g Product $ \One z ->
         (c A z, c B z)
